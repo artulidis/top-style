9b7b2f48d4ae9b28bebe526c5eb45804c8094cbd
artulidis <artulidis@gmail.com>
2025-10-29 16:46:16 -0400

    Functioning admin edit logic

--- app.py
+++ app.py
@@ -1,8 +1,58 @@
-from flask import Flask, redirect, render_template, abort, request
+import os, json
+from flask import Flask, jsonify, redirect, render_template, abort, request
 from jinja2 import TemplateNotFound
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
+
+# Admin Logic
+
+CONTENT_FILE = "content.json"
+
+def load_content():
+    if os.path.exists(CONTENT_FILE):
+        with open(CONTENT_FILE, "r", encoding="UTF-8") as f:
+             return json.load(f)
+    
+    return {}
+
+def save_content(data):
+     with open(CONTENT_FILE, "w", encoding="UTF-8") as f:
+        json.dump(data, f, indent=2, ensure_ascii=False)
+
+
+@app.context_processor
+def inject_cms():
+    return {"cms": load_content()}
+
+@app.route("/admin/save", methods=["POST"])
+def admin_save():
+    """
+        Expected Payload:
+        {
+        "page": "index",
+        "changes": {
+            "hero.h1": "New Hero Heading",
+            "services.price_1": "45"
+            }
+        }
+    """
+
+    payload = request.get_json() or {}
+    page = payload.get("page")
+    changes = payload.get("changes", {})
+
+    content = load_content()
+
+    if page not in content:
+        content[page] = {}
+    
+    content[page].update(changes)
+    save_content(content)
+    return jsonify({"status": "OK", "saved": len(changes)}), 200  
+
+
+# Routing Logic
 
 @app.before_request
 def clear_trailing_slashes():
